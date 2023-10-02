39be1f6c16a7910eb81c62c8fafc75c40fb2cfd0
Lord Ghapa <felipef32@hotmail.com>
2023-10-01 23:18:45 -0300

    ✨ Feat: create initial hero section

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,33 @@
+export default function Home() {
+  return (
+    <main className="  flex flex-col bg-center text-white ">
+      <section className="hero flex h-screen items-center justify-center bg-hero  bg-cover bg-no-repeat px-3 pt-10  md:max-h-[700px]">
+        <div
+          id="wrapper"
+          className="md:align-center flex h-full  w-full  max-w-[1200px] flex-col justify-center gap-10 md:items-center lg:flex-row lg:justify-between"
+        >
+          <div className="flex max-w-[520px] flex-col">
+            <p className="font-lato md:text-2xl md:tracking-wide">
+              Transformando ideias em experiências digitas
+            </p>
+            <p className="mb-4 font-lato text-2xl tracking-wider md:text-5xl md:tracking-wide">
+              Desenvolvedor Front-end
+            </p>
+            <p className="font-lato">
+              Olá, eu sou Felipe Lacerda, a minha jornada no mundo do
+              desenvolvimento web começou com o React e o Next.js, e desde então
+              tenho trabalhado para aprimorar minhas habilidades e criar sites e
+              aplicativos envolventes.
+            </p>
+          </div>
+
+          <img
+            className="max-h-[289px] w-full max-w-[473px] "
+            src="/pc.png"
+            alt="Sou uma imagem"
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,37 @@
+const Hero = () => {
+  return (
+    <main>
+      <section
+        id="hero"
+        className="h-full bg-heromd bg-cover px-3 py-20 text-white md:bg-hero"
+      >
+        <div
+          id="wrapper"
+          className="mx-auto flex w-full max-w-[500px] flex-col items-center md:max-w-3xl lg:max-w-[1200px] lg:flex-row lg:justify-between "
+        >
+          <div className="lg:max-w-[550px]">
+            <p className=" font-lato sm:text-lg md:text-center md:text-2xl md:tracking-wide lg:text-left">
+              Transformando ideias em experiências digitas
+            </p>
+            <p className=" mb-4 font-lato text-2xl tracking-wider sm:text-4xl md:text-center md:text-5xl md:tracking-wide lg:text-left">
+              Desenvolvedor Front-end
+            </p>
+            <p className="mb-5 font-lato sm:text-lg lg:text-base">
+              Olá, eu sou Felipe Lacerda, a minha jornada no mundo do
+              desenvolvimento web começou com o React e o Next.js, e desde então
+              tenho trabalhado para aprimorar minhas habilidades e criar sites e
+              aplicativos envolventes.
+            </p>
+          </div>
+          <img
+            className="h-auto max-h-[289px] w-full max-w-[473px] "
+            src="/pc.png"
+            alt="Sou uma imagem"
+          />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Hero;
