c9f3bb04cc7d7481062e361287d75f22e2f34d46
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2023-11-30 23:09:46 -0300

    💄 UI: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import About from "@/components/page2/About";
 import Hero from "@/components/page2/Hero";
 import Projects from "@/components/page2/projects";
-// import Skills from "@/components/page2/Skills";
+import Skills from "@/components/page2/Skills";
 
 const Home = () => {
   return (
@@ -9,7 +9,7 @@ const Home = () => {
       <main className="  z-20 h-full w-full">
         <div className="z-20 flex flex-col ">
           <Hero />
-          {/* <Skills /> */}
+          <Skills />
           <Projects />
           <About />
         </div>
--- src/components/page2/projects/index.tsx
+++ src/components/page2/projects/index.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
         <h2 className="text-clip bg-gradient-to-r from-purple-500 to-cyan-500 bg-clip-text pb-20 text-5xl font-semibold text-transparent">
           Projetos
         </h2>
-        <div className="cardsComponent  flex h-full w-full	max-w-[1536px] flex-col flex-wrap items-center justify-center gap-10  lg:flex-row">
+        <div className="cardsComponent  flex h-full w-full	max-w-[1536px] flex-col flex-wrap items-center justify-center gap-10 px-2  lg:flex-row">
           {data.map((card, index) => (
             <React.Fragment key={index}>
               <ProjectCard
